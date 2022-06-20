397e4ad9a39e561183e5985b97c567955c7ec619
R252-252\Owner <2022_k-oda_yaz@it-college.co.jp>
2022-06-20 11:31:08 +0900

    LoginAction,SearchService変更

--- OkinawaGo/src/main/java/com/rhizome/example/service/SearchService.java
+++ OkinawaGo/src/main/java/com/rhizome/example/service/SearchService.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 public class SearchService extends BaseService(){
 
 	public SearchService() throws Exception {
@@ -14,13 +15,14 @@ public class SearchService extends BaseService(){
 	 */
 	public Map<String, Object> doLogin(String mail, String pass) throws Exception {
 		Map<String, Object> loginData = new HashMap<>();
+		//DAOによって変更
 		EmployeeDao dao = new EmployeeDao(this.con);
 		try {
 			loginData.put("EMP", dao.selectLoginInfo(mail, pass));
-			loginData.put("DEP_LIST", this.getDepartmentList());
 		} finally {
 			DbUtil.closeConnection(this.con);
 		}
 		return loginData;
 	}
+	
 }
