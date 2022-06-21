c7d1d14321af3765386e735ed42033a794dfa8e1
R252-252\Owner <2022_k-oda_yaz@it-college.co.jp>
2022-06-21 14:24:20 +0900

    ShowInfo作成

--- OkinawaGo/src/main/java/com/rhizome/example/userpage/ShowInfoAction.java
+++ OkinawaGo/src/main/java/com/rhizome/example/userpage/ShowInfoAction.java
@@ -4,18 +4,22 @@ import javax.servlet.annotation.WebServlet;
 
 @WebServlet(name = "select", urlPatterns = { "/select" })
 public class ShowInfoAction extends BaseServlet {
-	
+	//画像から詳細データを取得
 	protected String doAction() throws Exception {
-		String pageParam = super.getInputParameter(
+		String[] pageParam = super.getInputParameter(
 				"info_img"
+				,"info_id"
 				
-				null null = new null
-				null.set null(pageParam);
+				 null img = new null();
+		        img.setnull(pageParam[0]);
+		        img.setnull(pageParam[1]);
 				
 				SearchService service = new SearchService();
-				List<null> imgList = searchDetailBykey(pageParam);
+				
+				List<null> imgList = service.searchDetailBykey(pageParam[0],pageParam[1]);
 				
 				super.request.setAttribute("imgList", imgList);
+				
 				return "detail";
 	}
 }
