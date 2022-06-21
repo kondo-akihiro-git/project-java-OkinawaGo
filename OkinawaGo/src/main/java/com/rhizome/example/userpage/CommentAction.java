c16c1fbad2303abccda4d25b9ed23266f2e8217b
R252-252\Owner <2022_k-oda_yaz@it-college.co.jp>
2022-06-21 13:28:57 +0900

    修正

--- OkinawaGo/src/main/java/com/rhizome/example/userpage/CommentAction.java
+++ OkinawaGo/src/main/java/com/rhizome/example/userpage/CommentAction.java
@@ -5,13 +5,14 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(name = "select", urlPatterns = { "/select" })
 public class CommentAction extends BaseServlet {
 
+	//ユーザー名と投稿内容をデータベースに追加
 String[] pageParam = super.getInputParameter(
 		"comment_nm"
 		,"comment_tx"
 		
-		null null = new null
-		null.set null(pageParam[0]);
-		null.set null(pageParam[1]);
+		null info = new null;
+		info.set null(pageParam[0]);
+		info.set null(pageParam[1]);
 		
 		UpdateService service = new UpdateService();
 		List<null> commentList =  service.commentInput(pageParam[0],pageParam[1]);
