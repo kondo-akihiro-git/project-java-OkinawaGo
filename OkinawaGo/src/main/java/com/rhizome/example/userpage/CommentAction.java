4401f7568e70ad4b10b5428401269ac807705333
R294-294\Owner <2022_a-kondo_yaz@it-college.co.jp>
2022-06-22 17:43:31 +0900

    BaseServlet再追加、commentAction修正

--- OkinawaGo/src/main/java/com/rhizome/example/userpage/CommentAction.java
+++ OkinawaGo/src/main/java/com/rhizome/example/userpage/CommentAction.java
@@ -1,6 +1,6 @@
 package main.java.com.rhizome.example.userpage;
 
-import java.util.List;
+import java.sql.SQLException;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -16,20 +16,16 @@ public class CommentAction extends BaseServlet {
 		return null;
 	}
 
-	protected String doAction() {
+	protected String doAction() throws NumberFormatException, ClassNotFoundException, SQLException {
 
 		// ユーザー名と投稿内容をデータベースに追加
 		String info_id = super.request.getParameter("info_id");
-		String comment_id = super.request.getParameter("comment_id");
 		String comment_nm = super.request.getParameter("comment_nm");
 		String comment_tx = super.request.getParameter("comment_tx");
 		String comment_img = super.request.getParameter("comment_img");
 
-		Okinawa_DAO dao = new Okinawa_DAO();
-		List<comment_DTO> commentList = dao.CommentInsert(info_id, comment_id, comment_nm, comment_tx, comment_img);
-
-		super.request.setAttribute("commentList", commentList); // リストのままsetAttributeするかどうか放置(06/17)。
-
+		Okinawa_DAO dao = new Okinawa_DAO(null);
+		dao.CommentInsert(info_id, comment_nm, comment_tx, comment_img);
 		return "detail";
 	}
 
