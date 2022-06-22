2a6737690cbd99e7f8edadd61813a2ad338faefc
R294-294\Owner <2022_a-kondo_yaz@it-college.co.jp>
2022-06-22 14:56:01 +0900

    UpdateService削除、SearchService修正

--- OkinawaGo/src/main/java/com/rhizome/example/service/SearchService.java
+++ OkinawaGo/src/main/java/com/rhizome/example/service/SearchService.java
@@ -25,34 +25,20 @@ public class SearchService extends BaseService(){
 	}
 
 	/**
-	 * 管理者トップでフリーワードによるスポット・グルメ検索を行う---------------------------------------------------------------------------------
-	 */
-	public List<Info_DTO> searchInfo(String huri_wa_do) throws Exception {
-		Okinawa_DAO dao = new Okinawa_DAO(this.con);
-		List<Info_DTO> list = null;
-		try {
-			list = dao.selectByFreeWord(huri_wa_do)); //入力された値で条件検索
-			if (list.size() == 0) {
-				list = null;
-			}
-		} finally {
-			DbUtil.closeConnection(this.con);
-		}
-		return list;
-	}
-
-	/**
 	 * 管理者トップ画面からスポット・グルメ詳細ページに飛ぶ-------------------------------------------------------------------
 	 *　カテゴリーIDとスポット・グルメ名から詳細情報を取得するDAOメソッドをどうするか(06/22)。
 	 */
+	
+	//下のメソッドが必要かどうか要確認(06/22)
 	public List<Info_DTO> searchDetailBykey(Integer cateId, String name) throws Exception {
 		Okinawa_DAO dao = new Okinawa_DAO(this.con);
 		List<Info_DTO> AllList = null; // メソッドの戻り値のデータ型がわかっていません…ごめんなさい(06/20)。
 		List<Info_DTO> list = null;
 		try {
 			AllList = dao.selectAll(); // カテゴリーIDはInfo_mテーブルに含まれていないため対策案検討中(06/22)
-			while(AllList.contains(cateId) && AllList.contains(name)) {
-				list.add(AllList);
+			for(int i = 0; AllList>i; i++) {
+			if(AllList.get(i).contains(cateId) && AllList.get(i).contains(name)) {
+				list.add(AllList.get(i));
 			}
 			list = dao.findDetailByIdName(cateId, name); // カテゴリーIDと名前からスポット詳細情報を取得
 		} finally {
@@ -60,50 +46,4 @@ public class SearchService extends BaseService(){
 		}
 		return list;
 	}
-
-	/*
-	 * ユーザー検索画面からスポット・グルメ詳細ページに飛ぶ---------------------------------------------------
-	 * 選択された写真から詳細情報を取得するDAOメソッドをどうするか(06/22)
-	 * 
-	 */
-	public List<Info_DTO> searchDetailBykey(String info_id) throws Exception {
-		Okinawa_DAO dao = new Okinawa_DAO(this.con);
-		Info_DTO list = null; // メソッドの戻り値のデータ型がわかっていません…ごめんなさい(06/20)。
-		try {
-			list = dao.selectByInfoId(info_id); // カテゴリーIDと名前からグルメ詳細情報を取得
-		} finally {
-			DbUtil.closeConnection(this.con);
-		}
-		return list;
-	}
-
-	/**
-	 * 管理者トップ画面からコメント一覧ページに遷移-------------------------------------------------------------------
-	 */
-
-	public List<Comment_DTO> searchCommentBykey(String info_id) throws Exception {
-		Okinawa_DAO dao = new Okinawa_DAO(this.con);
-		List<Comment_DTO> list = null;
-		try {
-			list = dao.selectComment(info_id); // カテゴリーIDと名前からコメント一覧を取得
-		} finally {
-			DbUtil.closeConnection(this.con);
-		}
-		return list;
-	}
-
-	/**
-	 * ユーザー側でのグルメ検索画面からカテゴリー検索後、検索結果画面に遷移-------------------------------------------------------------------
-	 */
-	public List<Info_DTO> conditionalSearch(Integer area_id, String[] checkedbox) throws Exception {
-		Okinawa_DAO dao = new Okinawa_DAO(this.con);
-		List<Info_DTO> list = null;
-		try {
-			list = dao.GurumeSelect(area_id, checkbox); //地域IDとチェックボックスからスポット・グルメの一覧を取得
-		}finnaly {
-			DbUtil.closeConnection(this.con);
-		}
-		return list;
-	}
-
 }　
