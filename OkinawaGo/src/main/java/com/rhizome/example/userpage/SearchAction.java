26b39275d7c20e92f51e6794e3f74f2bff776111
R252-252\Owner <2022_k-oda_yaz@it-college.co.jp>
2022-06-22 15:00:31 +0900

    searchActionの修正

--- OkinawaGo/src/main/java/com/rhizome/example/userpage/SearchAction.java
+++ OkinawaGo/src/main/java/com/rhizome/example/userpage/SearchAction.java
@@ -2,12 +2,13 @@ package com.rhizome.example.userpage;
 
 @WebServlet(name = "search", urlPatterns = { "/search" })
 public class SelectAction extends BaseServlet {
+
 	public String doAction() {
 
 String text;
 String value;
 Okinawa_DAO dao = new Okinawa_DAO();
-List<null> infolist;
+List<Info_DTO> infolist;
 
 //検索トップ画面からフリーワードで検索した場合-------------------------------------------------------------
 if (super.request.getParameter("toptext") != null) {
@@ -23,25 +24,24 @@ if(super.request.getParameter("spottext") != null) {
 //スポット検索画面のマップで検索した場合
 }else if (super.request.getParameter("spotmap") != null) {
 	value = super.request.getParameter("spotmap");
-	infolist = dao.selectByFreeWord(value);
+	infolist = dao.selectByArea(value);
 }
 //グルメ検索画面からフリーワードで検索した場合--------------------------------------------------------------
-	if(super.request.getParameter("goutext")!=null)
-
-	{
-		text = super.request.getParameter("text");
-		infolist = dao.selectBy(text);
+	if(super.request.getParameter("goutext")!=null){
+		
+		text = super.request.getParameter("area");
+		infolist = dao.selectByFreeWord(text);
 
 //グルメ検索画面からカテゴリー検索した場合
-	}else if(super.request.getParameter("map_category"!=null)
+	}else if(super.request.getParameter("map_category") !=null)
 	{
 		int id = super.request.getParameter("id"); // ラジオボタンでチェックされた値を取得
 		if (super.request.getParameter("gou_category" != null)) {
 			String[] list = super.request.getParameterValues("checkbox"); // チェックボックスの値を配列で取得 //チェックボックスでチェックされた値を取得
 		}
-		infolist = service.conditionalSearch(id, list);
-	}if(infolist==null||infolist.size()==0)
-	{
+		infolist = dao.GurumeSelect(id, list);
+		
+	}if(infolist==null||infolist.size()==0){
 		throw new Exception("入力された条件で情報が見つかりませんでした");
 	}
 
