52aeddc53237e2e418f4358b8e18b9a105097ab0
R108-108\Owner <a-takeyama>
2022-06-23 14:31:43 +0900

    検索結果画面タイトルに必要なパラメータをセットしました。

--- OkinawaGo/src/main/java/com/rhizome/example/userpage/SearchAction.java
+++ OkinawaGo/src/main/java/com/rhizome/example/userpage/SearchAction.java
@@ -26,23 +26,27 @@ public class SearchAction extends BaseServlet {
 		if (super.request.getParameter("toptext") != null) {
 			text = super.request.getParameter("toptext");
 			infolist = dao.selectByFreeWord(text);
+			super.request.setAttribute("text",text);
 		}
 
 //スポット検索画面からフリーワードで検索した場合------------------------------------------------------------
 		if (super.request.getParameter("spottext") != null) {
 			text = super.request.getParameter("spottext");
 			infolist = dao.selectByFreeWord(text);
+			super.request.setAttribute("text",text);
 
 //スポット検索画面のマップで検索した場合
 		} else if (super.request.getParameter("spotmap") != null) {
 			value = super.request.getParameter("spotmap");
 			infolist = dao.selectByArea(value);
+			super.request.setAttribute("map_id",value);
 		}
 //グルメ検索画面からフリーワードで検索した場合--------------------------------------------------------------
 		if (super.request.getParameter("goutext") != null) {
 
 			text = super.request.getParameter("goutext");
 			infolist = dao.selectByFreeWord(text);
+			super.request.setAttribute("text",text);
 
 //グルメ検索画面からカテゴリー検索した場合
 		} else if (super.request.getParameter("category") != null) {
@@ -53,7 +57,8 @@ public class SearchAction extends BaseServlet {
 			List<Info_id_img_DTO> List = null;
 			List = dao.GurumeSelect(id, list);
 			super.request.setAttribute("infolist", List);
-
+			super.request.setAttribute("list",list);
+			super.request.setAttribute("id",id);
 		}
 		if (infolist == null || infolist.size() == 0) {
 			throw new Exception("入力された条件で情報が見つかりませんでした");
